efb561ea66e68a9e2d10b816ab53bfb6002cd614
Kidokiller87 <kidokiller87@gmail.com>
2021-01-03 21:44:49 +0300

    add Dialogs lesson21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
-        </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Valdemar
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sanya
+                </div>
+                <div className={s.dialog}>
+                    Viktar
+                </div>
+            </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>How are yoy</div>
+                    <div className={s.message}>Superpupper</div>
+
+                </div>
+            </div>
+
 
     )
 }
