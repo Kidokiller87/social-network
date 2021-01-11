1375403e41d354146ee7edc03ce7b3d211186f4b
Kidokiller87 <kidokiller87@gmail.com>
2021-01-11 14:43:48 +0300

    less51 - install axios, add GET request in Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,15 @@
 import React from "react";
 import styles from './users.module.css'
-
+import * as axios from "axios";
+import userPhoto from './../../assets/images/avatar.jpg'
 let Users = (props) => {
 
-if (props.users.length ===0) {
-    props.setUsers([{
-        id: 1,
-        photoUrl: 'https://i.pinimg.com/474x/db/65/48/db654830b510091b321a4d4d0160ee9e.jpg',
-        followed: false,
-        fullName: 'Vladimir',
-        status: 'I am boss',
-        location: {city: 'Minsk', country: 'Belarus'}
-    },
-        {
-            id: 2,
-            photoUrl: 'https://i.pinimg.com/474x/06/84/e3/0684e340839cc8b22d3d3a481ed162ea.jpg',
-            followed: true,
-            fullName: 'Dmitry',
-            status: 'Hello React',
-            location: {city: 'Moscow', country: 'Russia'}
-        },
-        {
-            id: 3,
-            photoUrl: 'https://as1.ftcdn.net/jpg/01/92/48/10/500_F_192481078_9pweL73rF0X5RFhPk5NFtpwmXvM0DpDC.jpg',
-            followed: false,
-            fullName: 'Lusi',
-            status: 'Super',
-            location: {city: 'New York', country: 'USA'}
-        }])
+if (props.users.length ===0 ) {
+
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then (response => {
+        props.setUsers(response.data.items)
+    });
+
 }
 
     return <div>
@@ -35,7 +17,7 @@ if (props.users.length ===0) {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -50,12 +32,12 @@ if (props.users.length ===0) {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                 <span>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+                <div>{'u.location.country'}</div>
+                <div>{'u.location.city'}</div>
                 </span>
                 </span>
             </div>)
--- src/components/redux/users-reducer.js
+++ src/components/redux/users-reducer.js
@@ -5,7 +5,30 @@ const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-
+        /*{
+            id: 1,
+            photoUrl: 'https://i.pinimg.com/474x/db/65/48/db654830b510091b321a4d4d0160ee9e.jpg',
+            followed: false,
+            fullName: 'Vladimir',
+            status: 'I am boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+        },
+        {
+            id: 2,
+            photoUrl: 'https://i.pinimg.com/474x/06/84/e3/0684e340839cc8b22d3d3a481ed162ea.jpg',
+            followed: true,
+            fullName: 'Dmitry',
+            status: 'Hello React',
+            location: {city: 'Moscow', country: 'Russia'}
+        },
+        {
+            id: 3,
+            photoUrl: 'https://as1.ftcdn.net/jpg/01/92/48/10/500_F_192481078_9pweL73rF0X5RFhPk5NFtpwmXvM0DpDC.jpg',
+            followed: false,
+            fullName: 'Lusi',
+            status: 'Super',
+            location: {city: 'New York', country: 'USA'}
+        }*/
 ]
 };
 
