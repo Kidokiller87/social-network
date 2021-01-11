653b4f7c1f63ea77a3d2eab868de64b894358049
Kidokiller87 <kidokiller87@gmail.com>
2021-01-11 21:49:01 +0300

    less54 - add componentDidMount in Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,13 +4,11 @@ import * as axios from "axios";
 import userPhoto from './../../assets/images/avatar.jpg'
 
 class Users extends React.Component {
-  constructor(props) {
-      super(props);
-              axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+       componentDidMount() {
+          axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
               this.props.setUsers(response.data.items)
           });
       }
-
 
     render() {
         return <div>
