a309fb699c1c25e89410b80064300f56caab6d3b
Kidokiller87 <kidokiller87@gmail.com>
2021-01-12 22:58:29 +0300

    less60 - add withRouter in ProfileContainer.jsx

--- src/components/Profile/Profileinfo/ProfileInfo.jsx
+++ src/components/Profile/Profileinfo/ProfileInfo.jsx
@@ -14,9 +14,16 @@ const ProfileInfo = (props) => {
             <div>
                 <img src="https://t4.ftcdn.net/jpg/03/76/63/37/360_F_376633784_QFdVNtPwVroiMwK3Wn33D4HQp0AiVIkl.jpg"/>
             </div>
+
             <div className = {s.descriptionBlock}>
               <img src={props.profile.photos.large}/>
-                ava + decscription
+                <div>{props.profile.contacts.facebook}</div>
+                <div>{props.profile.contacts.vk}</div>
+                <div>{props.profile.contacts.twitter}</div>
+                <div>{props.profile.contacts.github}</div>
+                <div>{props.profile.fullName}</div>
+                <div>ava + decscription</div>
+
             </div>
 
         </div>
