23c25b1d77c8cf82f33ab48db1985d6cf07ccfc9
Kidokiller87 <kidokiller87@gmail.com>
2021-01-27 22:59:46 +0300

    less87 - add React.memo in MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,15 +6,14 @@ import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormsControls/FormsControls";
 
 
+const MyPosts = React.memo(props => {
+    console.log('render yo')
+    let postsElements =
+        props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
-const MyPosts = (props) => {
-
-let postsElements =
-    props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>)
-
-let onAddPost = (values) => {
-   props.addPost(values.newPostText);
- }
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText);
+    }
 
     return (
         <div className={s.postsBlock}>
@@ -25,7 +24,8 @@ let onAddPost = (values) => {
             </div>
         </div>
     )
-}
+})
+
 const maxLength10 = maxLengthCreator(10)
 
 let AddNewPostForm = (props) => {
