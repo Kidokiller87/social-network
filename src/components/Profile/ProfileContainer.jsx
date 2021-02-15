6207d2e2d5b5b9f1d229cb67bcabfcc6857b54b2
Kidokiller87 <kidokiller87@gmail.com>
2021-02-15 22:13:32 +0300

    less96 - add shouldComponentUpdate in ProfileContainer.jsx, add photoselector in ProfileInfo.jsx

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {getProfile, getStatus, updateStatus} from "../redux/profile-reducer";
+import {getProfile, getStatus, savePhoto, updateStatus} from "../redux/profile-reducer";
 import {withRouter} from "react-router";
 import {compose} from "redux";
 
 
-
 class ProfileContainer extends React.Component {
 
+    refreshProfile() {
+        let userId = this.props.match.params.userId
+        if (!userId) {
+            userId = this.props.authorisedUserId;
+            if (!userId) {
+                this.props.history.push('/login')
+            }
+        }
+        this.props.getProfile(userId);
+        this.props.getStatus(userId)
+    }
+
    componentDidMount() {
-       let userId = this.props.match.params.userId
-       if (!userId) {
-           userId = this.props.authorisedUserId;
-           if (!userId) {
-               this.props.history.push('/login')
-           }
-       }
-       this.props.getProfile(userId);
-           this.props.getStatus(userId)
+       this.refreshProfile();
    }
+
+   componentDidUpdate(prevProps, prevState, snapshot) {
+       if (this.props.match.params.userId != prevProps.match.params.userId )
+       this.refreshProfile();
+   }
+
     render() {
 
         return (
                 <Profile {...this.props}
+                         isOwner={!this.props.match.params.userId}
                          profile={this.props.profile}
                          status = {this.props.status}
-                         updateStatus = {this.props.updateStatus}/>
+                         updateStatus = {this.props.updateStatus}
+                         savePhoto = {this.props.savePhoto}/>
         )
     }
 };
@@ -39,7 +50,7 @@ let mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth
 });
 
-export default compose(connect(mapStateToProps, {getProfile, getStatus, updateStatus}),
+export default compose(connect(mapStateToProps, {getProfile, getStatus, updateStatus,savePhoto}),
     withRouter/*,
     withAuthRedirect*/)(ProfileContainer);
 
--- src/components/Profile/Profileinfo/ProfileInfo.jsx
+++ src/components/Profile/Profileinfo/ProfileInfo.jsx
@@ -2,21 +2,28 @@ import React from "react";
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
-
-const ProfileInfo = ({profile, status,updateStatus}) => {
+import defaultAvatar from '../../../assets/images/avatar.jpg'
+const ProfileInfo = ({profile, status,updateStatus,isOwner, savePhoto}) => {
 
     if (!profile) {
         return <Preloader/>
     }
 
+    const onMainPhotoSelect = (e) => {
+if (e.target.files.length) {
+    savePhoto(e.target.files[0])
+}
+    }
+
     return (
         <div>
-            <div>
+           {/* <div>
                 <img src="https://i.pinimg.com/564x/3a/1d/b8/3a1db894b8ef2283230307ffe7cf31b3.jpg"/>
-            </div>
+            </div>*/}
 
             <div className = {s.descriptionBlock}>
-              <img src={profile.photos.large}/>
+              <img src={profile.photos.large || defaultAvatar} className={s.avatar}/>
+                {isOwner && <input type={'file'} onChange={onMainPhotoSelect}/>}
               <ProfileStatusWithHooks status = {status}
                              updateStatus = {updateStatus}/>
             </div>
